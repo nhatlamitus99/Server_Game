30b631facf5bdb5f93255c26b38158005047e2ad
Trung Hieu <>
2020-06-25 10:59:15 +0700

    init

---
+++ src/cmd/CmdDefine.java
@@ -0,0 +1,14 @@
+package cmd;
+
+
+public class CmdDefine {
+    public static final short CUSTOM_LOGIN = 1;
+    public static final short GET_USER_INFO = 1001;
+   
+    //Log cmd
+    public static final short MOVE = 2001;
+    public static final short GET_NAME = 2002;
+    public static final short SET_NAME = 2003;
+
+
+}
---
+++ src/event/handler/LoginSuccessHandler.java
@@ -0,0 +1,73 @@
+package event.handler;
+
+import bitzero.server.core.BZEvent;
+import bitzero.server.core.BZEventParam;
+import bitzero.server.core.BZEventType;
+import bitzero.server.core.IBZEvent;
+import bitzero.server.entities.User;
+import bitzero.server.extensions.BaseServerEventHandler;
+import bitzero.server.extensions.ExtensionLogLevel;
+
+import bitzero.util.ExtensionUtility;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import event.eventType.DemoEventParam;
+import event.eventType.DemoEventType;
+import model.PlayerInfo;
+import util.server.ServerConstant;
+
+public class LoginSuccessHandler extends BaseServerEventHandler {
+    public LoginSuccessHandler() {
+        super();
+    }
+
+    public void handleServerEvent(IBZEvent iBZEvent) {
+        this.onLoginSuccess((User) iBZEvent.getParameter(BZEventParam.USER));
+    }
+
+    /**
+     * @param user
+     * description: after login successful to server, core framework will dispatch this event
+     */
+    private void onLoginSuccess(User user) {
+        trace(ExtensionLogLevel.DEBUG, "On Login Success ", user.getName());
+        PlayerInfo pInfo = null;
+        try {
+            pInfo = (PlayerInfo) PlayerInfo.getModel(user.getId(), PlayerInfo.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (pInfo==null){
+            pInfo = new PlayerInfo(user.getId(), "username_" + user.getId());
+            try {
+                pInfo.saveModel(user.getId());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * cache playerinfo in RAM
+         */
+        user.setProperty(ServerConstant.PLAYER_INFO, pInfo);
+
+        /**
+         * send login success to client
+         * after receive this message, client begin to send game logic packet to server
+         */
+        ExtensionUtility.instance().sendLoginOK(user);
+        
+        /**
+         * dispatch event here
+         */
+        Map evtParams = new HashMap();
+        evtParams.put(DemoEventParam.USER, user);
+        evtParams.put(DemoEventParam.NAME, user.getName());
+        ExtensionUtility.dispatchEvent(new BZEvent(DemoEventType.LOGIN_SUCCESS, evtParams));
+
+    }
+
+}
